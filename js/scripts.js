51c1e253fd8b75312d54c9e66b8a6808985a7796
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-04-16 18:08:38 +0200

    Added provisional colors + Basic Styling of the body + Background img + Also added "For Loop" that lists all the pokemon in the array & highlights a pokemon over 0.6 tall.

--- js/scripts.js
+++ js/scripts.js
@@ -11,3 +11,12 @@ let pokemonList = [
             height: 0.5, 
             type:['water']}
 ];
+
+
+for (let i =0 ; i < pokemonList.length; i++){
+        if (pokemonList[i].height > 0.6){
+                document.write(pokemonList[i].name + ' ' + '(height:'+' '+pokemonList[i].height + ')'+' Wow thats a big one'+'<br>');
+        }else {
+                document.write(pokemonList[i].name + ' ' + '(height:'+' '+pokemonList[i].height + ')'+'<br>')
+        }
+} 
