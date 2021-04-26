93fea6b80e88d519db4e6c2c74c0d8c5b773cdb8
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-04-26 11:51:02 +0200

    Added Modal, Modal Styling, Added ReadMe

---
+++ README.md
@@ -0,0 +1,5 @@
+Built a small JS WebApp based in the popular show/game Pokemon. It's an online pokedex.
+
+Used HTML + CSS + JavaScript and connected my site to an API
+
+API: https://pokeapi.co/api/v2/pokemon/
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let pokemonRepository =  (function (){
         let pokemonList = []
         let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-        
+        let modalContainer = document.querySelector('#modal-container');
 
 
         /* Loop creating pokemon buttons*/
@@ -59,9 +59,61 @@ let pokemonRepository =  (function (){
                 
         function showDetails(item) {
                 pokemonRepository.loadDetails(item).then(function () {
-                  console.log(item);
+                  showModal(item);
                 });
                 }
+
+                /* modal */
+                function showModal(pokemon, text) {
+                  modalContainer.innerHTML = '';
+                  let modal = document.createElement('div');
+                  modal.classList.add('modal');
+              
+                  let closeButtonElement = document.createElement('button');
+                  closeButtonElement.classList.add('modal-close');
+                  closeButtonElement.innerText = 'Close';
+                  closeButtonElement.addEventListener('click', hideModal);
+              
+                  let titleElement = document.createElement('h1');
+                  titleElement.innerText = pokemon.name;
+              
+                  let contentElement = document.createElement('p');
+                  contentElement.innerText = 'Height: ' +pokemon.height;
+
+                  let imageElement = document.createElement('img');
+                  imageElement.src = pokemon.imageUrl;
+              
+                  modal.appendChild(closeButtonElement);
+                  modal.appendChild(titleElement);
+                  modal.appendChild(contentElement);
+                  modal.appendChild(imageElement);
+                  modalContainer.appendChild(modal);
+              
+              
+                  modalContainer.classList.add('is-visible');
+                }
+              
+                function hideModal() {
+                  modalContainer.classList.remove('is-visible');
+                }
+              
+                window.addEventListener('keydown', (e) => {
+                  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+                    hideModal();  
+                  }
+                });
+                modalContainer.addEventListener('click', (e) => {
+                  let target = e.target;
+                  if (target === modalContainer) {
+                    hideModal();
+                  }
+                });
+              
+                document.querySelector('#show-modal').addEventListener('click', () => {
+                  showModal('Modal title', 'This is the modal content!');
+                });
+
+
 
         return {
                 add: add,
