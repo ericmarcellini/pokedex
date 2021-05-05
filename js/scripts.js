c105da1dbec1c2ef2d1b474ec5dc5672ef67adcc
ericmarcellini <80685530+ericmarcellini@users.noreply.github.com>
2021-05-05 18:00:51 +0200

    Added UL, Buttons displaying but not working properly*Still Broken*

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 let pokemonRepository =  (function (){
-        let pokemonList = []
-        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-        let modalContainer = document.querySelector('#modal-container');
-
+        let pokemonList = [];
+        let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
         /* Loop creating pokemon buttons*/
         function addListItem(pokemon){
                 let pokemonList = document.querySelector(".list-group");
-                let listpokemon = document.createElement("li");
-                listpokemon.classList.add('list-group-item', 'list-group-item-action')
+                let listPokemon = document.createElement("li");
+                listPokemon.classList.add('list-group-item', 'list-group-item-action');
                 let button = document.createElement("button");
 
                 button.innerText = pokemon.name;
                 button.classList.add("btn","btn-block");
-                button.setAttribute('data-target', 'data-toggle', 'modal')
+                button.setAttribute('data-target', 'data-toggle', 'modal', '#pokemonModal' );
 
-                listpokemon.appendChild(button);
-                pokemonList.appendChild(listpokemon);
+                pokemonList.appendChild(listPokemon);
+                listPokemon.appendChild(button);
                 button.addEventListener('click', function () {
                         showDetails(pokemon)
                       });
@@ -36,10 +34,10 @@ let pokemonRepository =  (function (){
                 return fetch(apiUrl).then(function (response) {
                   return response.json();
                 }).then(function (json) {
-                  json.results.forEach(function (pokemon) {
+                  json.results.forEach(function (item) {
                     let pokemon = {
-                      name: pokemon.name,
-                      detailsUrl: pokemon.url
+                      name: item.name,
+                      detailsUrl: item.url
                     };
                     add(pokemon);
                    });
@@ -60,10 +58,11 @@ let pokemonRepository =  (function (){
                   console.error(e);
                 });
                 }
+
                 function showDetails(pokemon) {
                   pokemonRepository.loadDetails(pokemon).then(function () {
-                    console.log(pokemon);
                     showModal(pokemon);
+                    console.log(pokemon);
                   })};
 
 
@@ -71,16 +70,13 @@ let pokemonRepository =  (function (){
         function showModal(pokemon) {
                   let modalBody = $('.modal-body');
                   let modalTitle = $('.modal-title');
-                  let modalHeader = $('.modal-header');
 
                   modalTitle.empty();
                   modalBody.empty();
 
                   let pokemonName = $("<h1>") + pokemon.name + $("</h1>");
                   let pokemonImage = $('<img class="modal-img" style="width:50%">');
-                  pokemonImage.attr("src", pokemon.imageUrlFront);
-
-                  pokemonImageBack.attr("src", pokemon.imageUrlBack);
+                  pokemonImage.attr("src", pokemon.imageUrl);
 
                   let pokemonHeight = $("<p>" + "Height: " + pokemon.height + "</p>")
                   let pokemonWeight = $("<p>" + "Weight: " + pokemon.weight + "</p>")
@@ -91,7 +87,6 @@ let pokemonRepository =  (function (){
 
                   modalTitle.append(pokemonName);
                   modalBody.append(pokemonImage);
-                  modalBody.append(pokemonImageBack);
                   modalBody.append(pokemonHeight);
                   modalBody.append(pokemonWeight);
                   modalBody.append(pokemonTypes);
