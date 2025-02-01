a7870ea1b69d2e17c26009f77a1c8e5052619029
ericmarcellini <delacresta@gmail.com>
2025-02-01 17:48:47 +0100

    creation of index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const IndexPage: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Pokedex</h1>
+            <p>Explore the world of Pokémon!</p>
+        </div>
+    );
+};
+
+export default IndexPage;
